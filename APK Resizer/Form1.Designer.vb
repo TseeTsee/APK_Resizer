6eca4e57c78140291e256303f3786bdac6c0cb9f
Spajky <marinspajic17@hotmail.com>
2012-11-22 19:53:39 +0100

    Initial Commit

---
+++ APK Resizer/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 118)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(360, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Resize!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(89, 11)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(202, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(297, 11)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Browse..."
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(12, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "APK file path:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Location = New System.Drawing.Point(14, 36)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(63, 26)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Existing" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "resolutions: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Location = New System.Drawing.Point(12, 75)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Resize to:"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton1.Enabled = False
+        Me.RadioButton1.Location = New System.Drawing.Point(70, 73)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(58, 17)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "XHDPI"
+        Me.RadioButton1.UseVisualStyleBackColor = False
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton2.Enabled = False
+        Me.RadioButton2.Location = New System.Drawing.Point(127, 73)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(51, 17)
+        Me.RadioButton2.TabIndex = 10
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "HDPI"
+        Me.RadioButton2.UseVisualStyleBackColor = False
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton3.Enabled = False
+        Me.RadioButton3.Location = New System.Drawing.Point(239, 73)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(52, 17)
+        Me.RadioButton3.TabIndex = 11
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "MDPI"
+        Me.RadioButton3.UseVisualStyleBackColor = False
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.BackColor = System.Drawing.Color.Transparent
+        Me.RadioButton4.Enabled = False
+        Me.RadioButton4.Location = New System.Drawing.Point(184, 73)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(49, 17)
+        Me.RadioButton4.TabIndex = 12
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "LDPI"
+        Me.RadioButton4.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.APK_Resizer.My.Resources.Resources.background
+        Me.ClientSize = New System.Drawing.Size(383, 157)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "APK Resizer by galaxyfreak"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton3 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton4 As System.Windows.Forms.RadioButton
+
+End Class
---
+++ APK Resizer/Form1.vb
@@ -0,0 +1,230 @@
+﻿' APK Resizer by galaxyfreak [http://forum.xda-developers.com/member.php?u=4635923]
+' Source code at: github.com/galaxyfreak
+'________________________________________________________________________________________
+
+' I know this code is a sloppy and mess but it works :P
+
+Imports ComponentAce.Compression.ZipForge
+Imports ComponentAce.Compression.Archiver
+Imports System.IO
+Imports System.Collections
+Imports System.Drawing
+Imports System.Drawing.Drawing2D
+
+Public Class Form1
+    Dim archiver As New ZipForge()
+    Dim APKopen As New OpenFileDialog
+    Dim temppath As String = "C:\temp"
+    Dim highestres As String
+    Dim targetres As String
+    Dim highestreslevel As Integer
+    Dim targetreslevel As Integer
+    Dim OriginalHeight As Single
+    Dim OriginalWidth As Single
+    Dim NewWidth As Integer
+    Dim NewHeight As Integer
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ExtractArchive(TextBox1.Text)
+        If Label3.Text.Contains("XHDPI") Then
+            highestres = "xhdpi"
+            highestreslevel = 4
+        ElseIf Label3.Text.Contains("HDPI") Then
+            highestres = "hdpi"
+            highestreslevel = 3
+        ElseIf Label3.Text.Contains("MDPI") Then
+            highestres = "mdpi"
+            highestreslevel = 2
+        ElseIf Label3.Text.Contains("LDPI") Then
+            highestres = "ldpi"
+            highestreslevel = 1
+        End If
+
+        If RadioButton1.Checked = True Then
+            targetres = "xhdpi"
+            targetreslevel = 4
+        ElseIf RadioButton2.Checked = True Then
+            targetres = "hdpi"
+            targetreslevel = 3
+        ElseIf RadioButton3.Checked = True Then
+            targetres = "mdpi"
+            targetreslevel = 2
+        ElseIf RadioButton4.Checked = True Then
+            targetres = "ldpi"
+            targetreslevel = 1
+        End If
+        If targetreslevel > highestreslevel Then
+            MsgBox("Target resolution is higher than original resolution." & vbNewLine & "This can cause pixelated images." & vbNewLine & "Are you sure you want to continue?", MsgBoxStyle.YesNo, "Warning!")
+        End If
+        ResizeImagesInFolder(temppath & "\res\drawable-" & highestres & "\", temppath & "\res\drawable-" & targetres & "\")
+        ZipAPK()
+    End Sub
+
+    Private Sub ExtractArchive(ByVal ExtractPath As String)
+        Try
+            archiver.FileName = TextBox1.Text
+            archiver.OpenArchive(System.IO.FileMode.Open)
+            archiver.BaseDir = ExtractPath
+            archiver.ExtractFiles("*.*")
+            archiver.CloseArchive()
+        Catch ex As ArchiverException
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim result As DialogResult = APKopen.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = APKopen.FileName
+            Label3.Text = "Existing" & vbNewLine & "resolutions:"
+            ExtractArchive(temppath)
+            RadioButton1.Enabled = True
+            RadioButton2.Enabled = True
+            RadioButton3.Enabled = True
+            RadioButton4.Enabled = True
+            If Directory.Exists(temppath & "\res\drawable-xhdpi") Then
+                Label3.Text = Label3.Text & " | XHDPI"
+                RadioButton1.Enabled = False
+            End If
+            If Directory.Exists(temppath & "\res\drawable-hdpi") Then
+                Label3.Text = Label3.Text & " | HDPI"
+                RadioButton2.Enabled = False
+            End If
+            If Directory.Exists(temppath & "\res\drawable-mdpi") Then
+                Label3.Text = Label3.Text & " | MDPI"
+                RadioButton3.Enabled = False
+            End If
+            If Directory.Exists(temppath & "\res\drawable-ldpi") Then
+                Label3.Text = Label3.Text & " | LDPI"
+                RadioButton4.Enabled = False
+            End If
+        End If
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim diag As New FolderBrowserDialog()
+        diag.Description = "Please select a folder to extract the current archive to."
+        diag.RootFolder = Environment.SpecialFolder.MyComputer
+        diag.ShowNewFolderButton = True
+
+        Dim result As DialogResult = diag.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            TextBox1.Text = diag.SelectedPath
+        End If
+    End Sub
+
+
+
+    Public Sub ResizeImagesInFolder(ByVal SourceFolder As String, ByVal DestinationFolder As String)
+        If Not Directory.Exists(SourceFolder) Then
+            Throw New Exception("SourceFolder does not exist")
+        End If
+        If Not Directory.Exists(DestinationFolder) Then
+            Directory.CreateDirectory(DestinationFolder)
+        End If
+        Dim diImages As DirectoryInfo = New DirectoryInfo(SourceFolder)
+        Dim alImages As ArrayList = New ArrayList()
+        alImages.AddRange(diImages.GetFiles("*.png"))
+        alImages.AddRange(diImages.GetFiles("*.9.png"))
+
+        Dim imgOriginal As Image
+        Dim ResizedBitmap As Bitmap
+        Dim ResizedImage As Graphics
+
+        For Each fiImage As FileInfo In alImages
+            imgOriginal = Image.FromFile(fiImage.FullName)
+            OriginalHeight = imgOriginal.Height
+            OriginalWidth = imgOriginal.Width
+            CheckSize()
+            ResizedBitmap = New Bitmap(NewWidth, NewHeight)
+            ResizedImage = Graphics.FromImage(ResizedBitmap)
+            ResizedImage.InterpolationMode = InterpolationMode.HighQualityBicubic
+            ResizedImage.CompositingQuality = CompositingQuality.HighQuality
+            ResizedImage.SmoothingMode = SmoothingMode.HighQuality
+            ResizedImage.DrawImage(imgOriginal, 0, 0, NewWidth, NewHeight)
+            ResizedBitmap.Save(DestinationFolder + fiImage.Name)
+            imgOriginal.Dispose()
+            ResizedBitmap.Dispose()
+            ResizedImage.Dispose()
+        Next
+    End Sub
+
+    Sub ZipAPK()
+        Dim apkpath As String
+        Try
+            apkpath = System.IO.Path.GetDirectoryName(APKopen.FileName)
+            archiver.FileName = apkpath & "\resized_" & APKopen.SafeFileName
+            archiver.OpenArchive(System.IO.FileMode.Create)
+            archiver.BaseDir = temppath
+            archiver.AddFiles("*.*")
+            archiver.CloseArchive()
+            Directory.Delete("C:\temp", True)
+        Catch ex As ArchiverException
+
+        End Try
+    End Sub
+
+    Sub CheckSize()
+
+        ' ------ resizing TO LDPI isn't done yet! --------
+
+        If highestres = "xhdpi" And targetres = "hdpi" Then
+            NewHeight = OriginalHeight - (OriginalHeight * 0.666)
+            NewWidth = OriginalWidth - (OriginalWidth * 0.666)
+        End If
+
+        If highestres = "xhdpi" And targetres = "mdpi" Then
+            NewHeight = OriginalHeight - (OriginalHeight * 0.444)
+            NewWidth = OriginalWidth - (OriginalWidth * 0.444)
+        End If
+
+        'If highestres = "xhdpi" And targetres = "ldpi" Then
+        'NewHeight = OriginalHeight - (OriginalHeight * resizepercentage)
+        'NewWidth = OriginalWidth - (OriginalWidth * resizepercentage)
+        'End If
+
+        If highestres = "hdpi" And targetres = "xhdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 1.5)
+            NewWidth = OriginalWidth + (OriginalWidth * 1.5)
+        End If
+
+        If highestres = "hdpi" And targetres = "mdpi" Then
+            NewHeight = OriginalHeight - (OriginalHeight * 0.666)
+            NewWidth = OriginalWidth - (OriginalWidth * 0.666)
+        End If
+
+        'If highestres = "hdpi" And targetres = "ldpi" Then
+        'NewHeight = OriginalHeight - (OriginalHeight * resizepercentage)
+        'NewWidth = OriginalWidth - (OriginalWidth * resizepercentage)
+        'End If
+
+        If highestres = "mdpi" And targetres = "xhdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 2.25)
+            NewWidth = OriginalWidth + (OriginalWidth * 2.25)
+        End If
+
+        If highestres = "mdpi" And targetres = "hdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 1.5)
+            NewWidth = OriginalWidth + (OriginalWidth * 1.5)
+        End If
+
+        'If highestres = "mdpi" And targetres = "ldpi" Then
+        'NewHeight = OriginalHeight - (OriginalHeight * resizepercentage)
+        'NewWidth = OriginalWidth - (OriginalWidth * resizepercentage)
+        'End If
+
+        If highestres = "ldpi" And targetres = "xdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 2.666)
+            NewWidth = OriginalWidth + (OriginalWidth * 2.666)
+        End If
+
+        If highestres = "ldpi" And targetres = "hdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 2)
+            NewWidth = OriginalWidth + (OriginalWidth * 2)
+        End If
+        If highestres = "ldpi" And targetres = "mdpi" Then
+            NewHeight = OriginalHeight + (OriginalHeight * 1.333)
+            NewWidth = OriginalWidth + (OriginalWidth * 1.333)
+        End If
+    End Sub
+End Class
